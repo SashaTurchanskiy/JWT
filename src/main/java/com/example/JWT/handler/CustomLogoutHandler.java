3625cadbecf6a87f62ad42bc5c2dc1a2f94446f2
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-01-13 13:05:28 +0200

    fixed problem with classes component and added class Config for security

--- src/main/java/com/example/JWT/handler/CustomLogoutHandler.java
+++ src/main/java/com/example/JWT/handler/CustomLogoutHandler.java
@@ -6,7 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomLogoutHandler implements LogoutHandler {
     private final TokenRepo tokenRepo;
 
--- src/main/java/com/example/JWT/models/Token.java
+++ src/main/java/com/example/JWT/models/Token.java
@@ -8,8 +8,6 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "token_table")
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Token {
@@ -23,10 +21,50 @@ public class Token {
     @Column(name = "refresh_token")
     private String refreshToken;
     @Column(name = "is_logged_out")
+    @Access(AccessType.FIELD)
     private boolean loggedOut;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public boolean isLoggedOut() {
+        return loggedOut;
+    }
+
+    public void setLoggedOut(boolean loggedOut) {
+        this.loggedOut = loggedOut;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }

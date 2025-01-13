04ae56d106fa43c2697c43c9a4031d213de04264
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-01-13 12:09:30 +0200

    realize few methods in JwtService for verify token, also added 3 classes for process userLogout

---
+++ src/main/java/com/example/JWT/handler/CustomLogoutHandler.java
@@ -0,0 +1,34 @@
+package com.example.JWT.handler;
+
+import com.example.JWT.models.Token;
+import com.example.JWT.repository.TokenRepo;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+public class CustomLogoutHandler implements LogoutHandler {
+    private final TokenRepo tokenRepo;
+
+    public CustomLogoutHandler(TokenRepo tokenRepo) {
+        this.tokenRepo = tokenRepo;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Authentication authentication) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        String token = authHeader.substring(7);
+        Token tokenEntity = tokenRepo.findByAccessToken(token).orElse(null);
+        if (tokenEntity != null) {
+            tokenEntity.setLoggedOut(true);
+            tokenRepo.save(tokenEntity);
+        }
+
+
+    }
+}
--- src/main/java/com/example/JWT/service/JwtService.java
+++ src/main/java/com/example/JWT/service/JwtService.java
@@ -1,12 +1,20 @@
 package com.example.JWT.service;
 
+import com.example.JWT.models.User;
 import com.example.JWT.repository.TokenRepo;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -16,16 +24,90 @@ public class JwtService {
     @Value("${security.jwt.access_token_expiration}")
     private long accessTokenExpiration;
     @Value("${security.jwt.access_token_expiration}")
-    private  long refreshTokenExpiration;
+    private long refreshTokenExpiration;
 
     private final TokenRepo tokenRepo;
 
     public JwtService(TokenRepo tokenRepo) {
         this.tokenRepo = tokenRepo;
     }
-    private SecretKey getSgningKey(){
+
+    private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String generateToken(User user, long expireTime) {
+        JwtBuilder builder = Jwts.builder()
+
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expireTime))
+                .signWith(getSigningKey());
+
+        return builder.compact();
+    }
+
+    public String generateAccessToken(User user) {
+        return generateToken(user, accessTokenExpiration);
+    }
+
+    public String generateRefreshToken(User user) {
+        return generateToken(user, refreshTokenExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        JwtParserBuilder parser = Jwts.parser();
+        parser.verifyWith(getSigningKey());
+
+        return parser.build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+
+        Claims claims = extractAllClaims(token);
+
+        return resolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isAccessTokenExpired(String token) {
+        return !extractExpiration(token).before(new Date());
+    }
+
+    public boolean isValid(String token, UserDetails user) {
+
+        String username = extractUsername(token);
+
+        boolean isValidToken = tokenRepo.findByAccessToken(token)
+                .map(t -> !t.isLoggedOut()).orElse(false);
+
+        return username.equals(user.getUsername())
+                && isAccessTokenExpired(token)
+                && isValidToken;
+    }
+
+    public boolean isValidRefresh(String token, User user) {
+
+        String username = extractUsername(token);
+
+        boolean isValidRefreshToken = tokenRepo.findByRefreshToken(token)
+                .map(t -> !t.isLoggedOut()).orElse(false);
+
+        return username.equals(user.getUsername())
+                && isAccessTokenExpired(token)
+                && isValidRefreshToken;
     }
 }
 
